b20b82887a79b7bbeff3610099eb258239a2be93
itzayush099 <116187797+itzayush099@users.noreply.github.com>
2026-06-07 14:20:47 +0530

    Configure PostgreSQL support for Vercel

--- myproject/settings.py
+++ myproject/settings.py
@@ -80,12 +80,17 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+if os.getenv('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.parse(os.getenv('DATABASE_URL'), conn_max_age=600)
 
 
 # Password validation
